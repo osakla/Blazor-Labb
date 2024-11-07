1c1cb9bcf97532e831c7dd6d6a97cb518b4c2c4b
osakla <osamaklay@gmail.com>
2024-11-07 21:06:47 +0100

    Add new user

---
+++ BlazorLabb/Models/Adress.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorLabb.Models
+{
+    public class Adress
+    {
+        public string? Street { get; set; }
+        public string? ZipCode { get; set; }
+        public string? City { get; set; }
+    }
+}
---
+++ BlazorLabb/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorLabb.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public Adress? Adress { get; set; } = new Adress();
+        public Company? Company { get; set; } = new Company();
+    }
+}
